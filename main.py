30f704e1ba3460762770dd74c4e25aa5b5e475ff
Virangya <guitarlass@gmail.com>
2024-09-25 21:54:59 +0530

    more editd

--- main.py
+++ main.py
@@ -8,22 +8,18 @@ while True:
     print("Searching for trees...")
 
     try:
-        # Load the images from the folder
-        file_path1 = os.path.join(folder_path, 'tree7.PNG')
-        file_path2 = os.path.join(folder_path, 'tree4.PNG')
+        # file_path2 = os.path.join(folder_path, 'tree7.PNG')
+        # region2 = pyautogui.locateOnScreen(file_path2, confidence=0.8, region=(186, 530, 400, 400))
+        #
+        # if region2:
+        #     print(f"tree7.PNG is near {region2.left}, pressing space")
+        #     pyautogui.press('space')
 
-        # Locate the images on screen with confidence
-        region1 = pyautogui.locateOnScreen(file_path1, confidence=0.5)
-        region2 = pyautogui.locateOnScreen(file_path2, confidence=0.5)
+        file_path1 = os.path.join(folder_path, 'tree4.PNG')
+        region1 = pyautogui.locateOnScreen(file_path1, confidence=0.8, region=(186, 530, 400, 400))
 
-        # Check if tree7.PNG is found and if its left is near 500
-        if region1 and region1.left <= 500:
-            print(f"tree7.PNG found near left {region1.left}, pressing space")
-            pyautogui.press('space')
-
-        # Check if tree4.PNG is found and if its left is near 500
-        if region2 and region2.left <= 500:
-            print(f"tree4.PNG found near left {region2.left}, pressing space")
+        if region1:
+            print(f"tree4.PNG is near {region1.left}, pressing space")
             pyautogui.press('space')
 
     except Exception as e:
